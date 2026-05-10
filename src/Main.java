1be269825718979de077da50f045674c1232cb73
git-roge <rogelio.abellera@gmail.com>
2026-05-10 17:25:52 +0800

    Refactor if-else to switch

--- src/Main.java
+++ src/Main.java
@@ -20,152 +20,216 @@ public class Main {
 
         int choice = 0;
         while (choice != 9) {
+
             try {
-                System.out.println("Actions: \n" +
-                        "1. Add Book\n2. Add User\n3. Show book list\n4. Show Users\n5. Borrow book\n6. Select User\n7. Return book\n8. Show transactions\n9. Exit"
-                );
+                System.out.println("""
+                Actions:
+                1. Add Book
+                2. Add User
+                3. Show book list
+                4. Show Users
+                5. Borrow book
+                6. Select User
+                7. Return book
+                8. Show transactions
+                9. Exit
+                """);
+
                 choice = scanner.nextInt();
                 scanner.nextLine();
-            }catch (InputMismatchException e) {
+
+            } catch (InputMismatchException e) {
                 System.out.println("Invalid action");
                 scanner.nextLine();
                 continue;
             }
-            //Add book
-            if (choice == 1) {
-                System.out.print("Book name: ");
-                String newBookName = scanner.nextLine();
-                System.out.print("Book author: ");
-                String newBookAuthor = scanner.nextLine();
-                System.out.print("Book year: ");
-                int newBookYear = scanner.nextInt();
-                scanner.nextLine();
 
-                library.addBook(newBookName, newBookAuthor, newBookYear);
-            }
-            //Add user
-            if (choice == 2) {
-                System.out.print("User name: ");
-                String newUserName = scanner.nextLine();
+            switch (choice) {
 
-                library.addUser(newUserName);
-            }
-            //Show book list
-            if (choice == 3) {
-                for (Map.Entry<Integer, Book> entry : library.getBookList().entrySet()) {
-                    System.out.printf("bookId: %d, bookName: '%s', bookAuthor: '%s', bookYear: %d, status: '%s'\n",
-                            entry.getKey(), entry.getValue().getTitle(), entry.getValue().getAuthor(), entry.getValue().getYear(), entry.getValue().getStatus());
-                }
-            }
-            //Show users
-            if (choice == 4) {
-                for (Map.Entry<Integer, User> entry : library.getUserList().entrySet()) {
-                    System.out.printf("userId: %d, userName: '%s'\n",
-                            entry.getKey(), entry.getValue().getUserName()
-                    );
-                }
-            }
+                case 1:
+                    System.out.print("Book name: ");
+                    String newBookName = scanner.nextLine();
 
-            //borrow book
-            if (choice == 5) {
-                int borrowBookId, borrowUserId;
-                for (Book book : library.getAvailableBooks()){
-                    System.out.println(book.getBookId() + ". " + book.getTitle());
-                }
+                    System.out.print("Book author: ");
+                    String newBookAuthor = scanner.nextLine();
 
-                while (true) {
-                    System.out.print("Enter userId: ");
-                    borrowUserId = scanner.nextInt();
+                    System.out.print("Book year: ");
+                    int newBookYear = scanner.nextInt();
                     scanner.nextLine();
 
-                    if (!library.validateUser(borrowUserId)) {
-                        System.out.println("User doesn't exist");
-                    } else {
+                    library.addBook(newBookName, newBookAuthor, newBookYear);
+
+                    break;
+
+                case 2:
+                    System.out.print("User name: ");
+                    String newUserName = scanner.nextLine();
+
+                    library.addUser(newUserName);
+
+                    break;
+
+                case 3:
+                    for (Map.Entry<Integer, Book> entry : library.getBookList().entrySet()) {
+
+                        System.out.printf(
+                                "bookId: %d, bookName: '%s', bookAuthor: '%s', bookYear: %d, status: '%s'%n",
+                                entry.getKey(),
+                                entry.getValue().getTitle(),
+                                entry.getValue().getAuthor(),
+                                entry.getValue().getYear(),
+                                entry.getValue().getStatus()
+                        );
+                    }
+
+                    break;
+
+                case 4:
+                    for (Map.Entry<Integer, User> entry : library.getUserList().entrySet()) {
+
+                        System.out.printf(
+                                "userId: %d, userName: '%s'%n",
+                                entry.getKey(),
+                                entry.getValue().getUserName()
+                        );
+                    }
+
+                    break;
+
+                case 5:
+
+                    int borrowBookId;
+                    int borrowUserId;
+
+                    System.out.println("List of available books");
+
+                    for (Book book : library.getAvailableBooks()) {
+                        System.out.printf(
+                                "Id: %d | Book Name: %s%n",
+                                book.getBookId(),
+                                book.getTitle()
+                        );
+                    }
+
+                    while (true) {
+
+                        System.out.print("Enter userId: ");
+                        borrowUserId = scanner.nextInt();
+                        scanner.nextLine();
+
+                        if (!library.validateUser(borrowUserId)) {
+                            System.out.println("User doesn't exist");
+                        } else {
+                            break;
+                        }
+                    }
+
+                    while (true) {
+
+                        System.out.print("Enter bookId: ");
+                        borrowBookId = scanner.nextInt();
+                        scanner.nextLine();
+
+                        if (!library.validateBook(borrowBookId)) {
+                            System.out.println("Book doesn't exist.");
+                        } else {
+                            break;
+                        }
+                    }
+
+                    library.borrowBook(borrowBookId, borrowUserId);
+
+                    break;
+
+                case 6:
+
+                    int userId;
+
+                    while (true) {
+
+                        System.out.print("Enter user Id: ");
+                        userId = scanner.nextInt();
+                        scanner.nextLine();
+
+                        if (!library.validateUser(userId)) {
+                            System.out.println("User doesn't exist");
+                        } else {
+                            break;
+                        }
+                    }
+
+                    User user = library.getUserList().get(userId);
+
+                    if (user == null) {
+                        System.out.println("User doesn't exist.");
                         break;
                     }
-                }
 
-                while (true) {
-                    System.out.print("Enter bookId: ");
-                    borrowBookId = scanner.nextInt();
-                    scanner.nextLine();
+                    System.out.println(user);
 
-                    if (!library.validateBook(borrowBookId)) {
-                        System.out.println("Book doesn't exist.");
-                    } else {
-                        break;
+                    for (Book book : user.getBorrowedBooks()) {
+                        System.out.println(
+                                book.getBookId() + ". " + book.getTitle()
+                        );
                     }
-                }
 
-                library.borrowBook(borrowBookId, borrowUserId);
-            }
+                    break;
 
-            if (choice == 6) {
-                int userId;
+                case 7:
 
-                while (true) {
-                    System.out.print("Enter user Id: ");
-                    userId = scanner.nextInt();
-                    scanner.nextLine();
+                    int returnBookId;
+                    int returnUserId;
 
-                    if (!library.validateUser(userId)) {
-                        System.out.println("User doesn't exist");
-                    } else {
-                        break;
+                    while (true) {
+
+                        System.out.print("Enter userId: ");
+                        returnUserId = scanner.nextInt();
+                        scanner.nextLine();
+
+                        if (!library.validateUser(returnUserId)) {
+                            System.out.println("User doesn't exist");
+                        } else {
+                            break;
+                        }
                     }
-                }
 
-                User user = library.getUserList().get(userId);
+                    while (true) {
 
-                if (user == null) {
-                    System.out.println("User doesn't exist.");
-                    continue;
-                }
+                        System.out.print("Enter bookId: ");
+                        returnBookId = scanner.nextInt();
+                        scanner.nextLine();
 
-                System.out.println(user.toString());
-                for (Book book : user.getBorrowedBooks()) {
-                    System.out.println(book.getBookId() + ". " + book.getTitle());
-                }
-            }
-            if (choice == 7) {
-                int returnBookId;
-                int returnUserId;
-
-                while (true) {
-                    System.out.print("Enter userId: ");
-                    returnUserId = scanner.nextInt();
-                    scanner.nextLine();
-
-                    if (!library.validateUser(returnUserId)) {
-                        System.out.println("User doesn't exist");
-                    } else {
-                        break;
+                        if (!library.validateBook(returnBookId)) {
+                            System.out.println("Book doesn't exist.");
+                        } else {
+                            break;
+                        }
                     }
-                }
 
-                while (true) {
-                    System.out.print("Enter bookId: ");
-                    returnBookId = scanner.nextInt();
-                    scanner.nextLine();
+                    library.returnBook(returnBookId, returnUserId);
 
-                    if (!library.validateBook(returnBookId)) {
-                        System.out.println("Book doesn't exist.");
-                    } else {
-                        break;
+                    break;
+
+                case 8:
+
+                    for (Transaction transaction : library.getTransactionList()) {
+                        System.out.println(transaction);
                     }
-                }
 
-                library.returnBook(returnBookId, returnUserId);
-            }
-            if (choice == 8) {
-                for (Transaction transaction : library.getTransactionList()) {
-                    System.out.println(transaction.toString());
-                }
-            }else {
-                System.out.println("Invalid input");
+                    break;
+
+                case 9:
+
+                    System.out.println("Library exit");
+
+                    break;
+
+                default:
+
+                    System.out.println("Invalid input");
             }
         }
 
-            System.out.println("Library exit");
+        System.out.println("Library exit");
         }
 }
